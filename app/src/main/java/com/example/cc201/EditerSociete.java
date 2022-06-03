19002c7184361b50d74071e8ca19305736bee6a3
Stagiaire <abdelilah.bakkali258@gmail.com>
2022-06-03 16:44:52 +0100

    List all societes with SimpleAdapter and layout without mainActivity named tous_societes.xml

--- app/src/main/java/com/example/cc201/EditerSociete.java
+++ app/src/main/java/com/example/cc201/EditerSociete.java
@@ -1,27 +1,31 @@
 package com.example.cc201;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class EditerSociete extends AppCompatActivity {
+    Spinner sp;
+    Mydb db;
+    EditText ed1,ed2,ed3;
+    ArrayList<Societe> societes;
+    ArrayAdapter n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editer_societe);
-        Spinner sp;
-        Mydb db;
-        EditText ed1,ed2,ed3;
-        ArrayList<Societe> societes;
-        ArrayAdapter n;
+
         db=new Mydb(this);
         sp=findViewById(R.id.sp);
         ed1=findViewById(R.id.ed1);
@@ -54,7 +58,62 @@ public class EditerSociete extends AppCompatActivity {
     }
 
     public void supprime(View view) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(EditerSociete.this);
+        alert.setTitle("Suppresion une societe ");
+        alert.setMessage("VOUS ETES VRAIMENT SUR ?");
+
+        alert.setPositiveButton("Supprime", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Societe p = societes.get(sp.getSelectedItemPosition());
+
+                if(Mydb.supprime_societe(db.getWritableDatabase(),p.getId())==-1)
+                    Toast.makeText(EditerSociete.this, "suppression echoue", Toast.LENGTH_SHORT).show();
+                else {
+                    Toast.makeText(EditerSociete.this, "Suppression avec succes reussie", Toast.LENGTH_SHORT).show();
+                    n.remove(p.getId() + " - " + p.getNom());
+                }
+            }
+        });
+
+        alert.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(EditerSociete.this, "votre tentative est annuler ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alert.show();
 
 
+    }
+
+    public void modifier(View view) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(EditerSociete.this);
+        alert.setTitle("Modifier la societe ");
+        alert.setMessage("Voulez-vous modifier cette entreprise  ?");
+        alert.setPositiveButton("Modifier", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Societe e = societes.get(sp.getSelectedItemPosition());
+
+                e.setNom(ed1.getText().toString());
+                e.setSecteurActivité(ed2.getText().toString());
+                e.setNombreemployé(Integer.valueOf(ed3.getText().toString()));
+
+                if(Mydb.modifier_societe(db.getWritableDatabase(),e)==-1)
+                    Toast.makeText(EditerSociete.this, "Modification echoue", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(EditerSociete.this, "Modification reussie", Toast.LENGTH_SHORT).show();
+            }
+        });
+        alert.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(EditerSociete.this, "vous etes annuler ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        alert.show();
     }
 }
--- app/src/main/java/com/example/cc201/ListeSocietes.java
+++ app/src/main/java/com/example/cc201/ListeSocietes.java
@@ -3,12 +3,35 @@ package com.example.cc201;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ListeSocietes extends AppCompatActivity {
+   ListView lst;
+    Mydb db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_societes);
+        lst=findViewById(R.id.transition_layout_save);
+        db=new Mydb(this);
+        db=new Mydb(this);
+        ArrayList<Societe> e =Mydb.recuperertous_societe(db.getReadableDatabase());
+        ArrayList<HashMap<String,Object>> h = new ArrayList<>();
+        for(Societe ec : e){
+            HashMap<String,Object> b =new HashMap<>();
+            b.put("Nom",ec.getNom());
+            b.put("nbemploye",ec.getNombreemployé());
+            h.add(b);
+        }
+        String[] from = {"Nom","nbemploye"};
+        int [] to ={R.id.txtnom,R.id.txtnbemploye};
+        SimpleAdapter b = new SimpleAdapter(this,h,R.layout.tous_societes,from,to);
+        lst.setAdapter(b);
+
     }
 }
