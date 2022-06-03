428a159036e57b225c9246bef6fe4e4b50932cf0
Stagiaire <abdelilah.bakkali258@gmail.com>
2022-06-03 16:06:46 +0100

    layout menu & style & layout AjouterSociete & btn ajout & btn annuler

---
+++ app/src/main/java/com/example/cc201/AjouterSociete.java
@@ -0,0 +1,40 @@
+package com.example.cc201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AjouterSociete extends AppCompatActivity {
+    EditText e1,e2,e3;
+    Mydb db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_societe);
+        db=new Mydb(this);
+        e1=findViewById(R.id.ed1);
+        e2=findViewById(R.id.ed2);
+        e3=findViewById(R.id.ed3);
+    }
+
+    public void annuler(View view) {
+        finish();
+    }
+
+    public void enregistrer(View view) {
+        Societe p = new Societe();
+
+        p.setNom(e1.getText().toString());
+        p.setSecteurActivité(e2.getText().toString());
+        p.setNombreemployé(Integer.valueOf(e3.getText().toString()));
+
+        if(Mydb.ajouter_societe(db.getWritableDatabase(),p)==-1)
+            Toast.makeText(this, "L'insertion echoue ", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "Insertion avec succes ", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/example/cc201/EditerSociete.java
@@ -0,0 +1,14 @@
+package com.example.cc201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditerSociete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editer_societe);
+    }
+}
